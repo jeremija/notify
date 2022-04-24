de80f703050cd635bd5c334f216098766088d129
Jerko Steiner <jerko.steiner@gmail.com>
2022-04-24 21:36:31 +0200

    telegram: Add markdown parse mode

--- service/telegram/telegram.go
+++ service/telegram/telegram.go
@@ -7,29 +7,50 @@ import (
 	"github.com/pkg/errors"
 )
 
-const defaultParseMode = tgbotapi.ModeHTML
+// ParseMode desecribes the message format.
+type ParseMode string
+
+const (
+	HTML     ParseMode = tgbotapi.ModeHTML
+	Markdown ParseMode = tgbotapi.ModeMarkdown
+)
 
 // Telegram struct holds necessary data to communicate with the Telegram API.
 type Telegram struct {
-	client  *tgbotapi.BotAPI
-	chatIDs []int64
+	client    *tgbotapi.BotAPI
+	chatIDs   []int64
+	parseMode ParseMode
 }
 
 // New returns a new instance of a Telegram notification service.
 // For more information about telegram api token:
 //    -> https://pkg.go.dev/github.com/go-telegram-bot-api/telegram-bot-api#NewBotAPI
-func New(apiToken string) (*Telegram, error) {
+func New(apiToken string, opts ...Opt) (*Telegram, error) {
 	client, err := tgbotapi.NewBotAPI(apiToken)
 	if err != nil {
 		return nil, err
 	}
 
 	t := &Telegram{
-		client:  client,
-		chatIDs: []int64{},
+		client:    client,
+		chatIDs:   []int64{},
+		parseMode: HTML,
+	}
+
+	for _, opt := range opts {
+		opt(t)
 	}
 
 	return t, nil
+}
+
+type Opt func(t *Telegram)
+
+// WithParseMode sets the message parse mode. Default is HTML.
+func WithParseMode(parseMode ParseMode) Opt {
+	return func(t *Telegram) {
+		t.parseMode = parseMode
+	}
 }
 
 // AddReceivers takes Telegram chat IDs and adds them to the internal chat ID list. The Send method will send
@@ -44,7 +65,7 @@ func (t Telegram) Send(ctx context.Context, subject, message string) error {
 	fullMessage := subject + "\n" + message // Treating subject as message title
 
 	msg := tgbotapi.NewMessage(0, fullMessage)
-	msg.ParseMode = defaultParseMode
+	msg.ParseMode = string(t.parseMode)
 
 	for _, chatID := range t.chatIDs {
 		select {
